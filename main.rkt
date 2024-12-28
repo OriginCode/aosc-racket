57816032603e3f84600ab125c63cda8f1c039f79
Kaiyang Wu <self@origincode.me>
2024-12-28 07:07:00 -0800

    chore(main): draw some nice wishes
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- main.rkt
+++ main.rkt
@@ -8,5 +8,36 @@
   (require rackunit
            rackunit/text-ui))
 
+;; Probably like this?
+;(define sample-package
+;  (define ver "1.0.0")
+;  (package
+;   (section "app-utils")
+;   (spec #:ver ver
+;         #:srcs (hash 'any (list
+;                              (git "https://github.com/sample/sample"
+;                                   #:options
+;                                   (list (copy-repo? #t)
+;                                         (commit (string-append "tags/v" ver)))))
+;                      'arm64 (list
+;                              (git "https://github.com/sample/sample-arm64"
+;                                   #:options
+;                                   (list (copy-repo? #t)
+;                                         (commit (string-append "tags/v" ver))))))
+;         #:chksums (list (skip))
+;         #:chkupdate (anitya 114514))
+;   (defines #:pkgname "sample"
+;            #:pkgdes "A sample package"
+;            #:pkgdep (list "glibc")
+;            #:builddep (list "meson" "ninja")
+;            #:pkgsec "utils")))
+
+(struct package (section spec defines))
+
+;; TODO: deploy package tree from a package variable
+(define/contract (deploy-package package)
+  (-> package? void?)
+  void)
+
 (module+ test
   (run-tests spec-tests))
