10048f3d51cd3627e99f29ef187b1ad0d2363d40
Kaiyang Wu <self@origincode.me>
2025-01-08 12:32:14 -0800

    chore(info): add package desc
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/aosc.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "AOSC Racket Packaging Tools")
 (define version "0.0")
 (define pkg-authors '(origincode))
 (define license '(Apache-2.0 OR MIT))
